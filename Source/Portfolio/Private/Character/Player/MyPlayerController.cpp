840cd7f5f7ec10b8b2fe4e78483de2452487a974
AengChoon <goldbugking91@gmail.com>
2023-12-27 22:34:51 +0900

    Player Controller

--- Source/Portfolio/Private/Character/Player/MyPlayerController.cpp
+++ Source/Portfolio/Private/Character/Player/MyPlayerController.cpp
@@ -2,4 +2,52 @@
 
 
 #include "Character/Player/MyPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AMyPlayerController::AMyPlayerController()
+{
+	bReplicates = true; 
+}
+
+void AMyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(PlayerContext);
+	
+	const auto InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(InputSubsystem);
+	InputSubsystem->AddMappingContext(PlayerContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputMode;
+	InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputMode.SetHideCursorDuringCapture(false);
+	SetInputMode(InputMode);
+}
+
+void AMyPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	const auto EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMyPlayerController::Move);
+}
+
+void AMyPlayerController::Move(const FInputActionValue& InInputActionValue)
+{
+	const auto InputAxisVector = InInputActionValue.Get<FVector2D>();
+	const FRotator YawRotation{0.0f, GetControlRotation().Yaw, 0.0f};
+	const FRotationMatrix YawRotationMatrix{YawRotation};
+
+	const FVector ForwardDirection = YawRotationMatrix.GetUnitAxis(EAxis::X);
+	const FVector RightDirection = YawRotationMatrix.GetUnitAxis(EAxis::Y);
+
+	if (const auto ControlledPawn = GetPawn())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
--- Source/Portfolio/Public/Character/Player/MyPlayerController.h
+++ Source/Portfolio/Public/Character/Player/MyPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "MyPlayerController.generated.h"
 
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -13,5 +16,21 @@ UCLASS()
 class PORTFOLIO_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AMyPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void Move(const FInputActionValue& InInputActionValue);
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> PlayerContext;
 	
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
 };
