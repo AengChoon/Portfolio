4c611b701f449066d3f44a9c4643547c692ceb80
AengChoon <goldbugking91@gmail.com>
2023-12-28 20:48:09 +0900

    Enemy Interface

--- Source/Portfolio/Private/Character/MyEnemy.cpp
+++ Source/Portfolio/Private/Character/MyEnemy.cpp
@@ -3,3 +3,6 @@
 
 #include "Character/MyEnemy.h"
 
+void AMyEnemy::HighlightActor(bool bInShouldHighlight)
+{
+}
--- Source/Portfolio/Public/Character/MyEnemy.h
+++ Source/Portfolio/Public/Character/MyEnemy.h
@@ -4,14 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Character/MyCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "MyEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class PORTFOLIO_API AMyEnemy : public AMyCharacterBase
+class PORTFOLIO_API AMyEnemy : public AMyCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor(bool bInShouldHighlight) override;
 };
